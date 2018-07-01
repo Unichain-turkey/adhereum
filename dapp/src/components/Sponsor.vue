ac18200fee6da9b148f72e46ded8f1a72547bc21
Fatih Altuntas <Altuntasfatih42@gmail.com>
2018-07-01 11:46:24 +0300

    Update

--- dapp/src/components/Sponsor.vue
+++ dapp/src/components/Sponsor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container" style="background: sandybrown">
+  <div class="container" style="background: floralwhite">
     <div class="row">
       <div class="col-md-3" v-for="sponsor in sponsors">
         <div class="thumbnail">
@@ -15,43 +15,46 @@
 
 <script>
 
-import store from '@/store/'
-import sponsorJson from '../../../build/contracts/Sponsor.json'
-function getSposnsor (address) {
-  let _web3 = store.getters.web3InstanceGetter()
-  return new _web3.eth.Contract(sponsorJson.abi, address)
-}
-export default {
-  name: "Sponsor",
-  data () {
-    return {
-      sponsors: []
-    }
-  },
-  mounted: function () {
-    var self = this
-    let _contract = this.$store.getters.contractInstance()
-    _contract.getPastEvents('beenSponsor', { fromBlock: 0, toBlock: 'latest' }, function(error, events) {
-      events.forEach((element) => {
-        var address=element.returnValues[0]
-        var _sponsor = getSposnsor(address)
-        _sponsor.methods.getSponsor().call().then(function (val) {
-          console.log(val)
-          self.sponsors.push({
-            "name":val[0],
-            "url":val[1],
-            "imageHash":val[2],
-            "duration":val[3],
-          })
-        })
-
-      });
-    })
-
-
-
+  import store from '@/store/'
+  import sponsorJson from '../../../build/contracts/Sponsor.json'
+  function getSposnsor (address) {
+    let _web3 = store.getters.web3InstanceGetter()
+    return new _web3.eth.Contract(sponsorJson.abi, address)
   }
-}
+  export default {
+    name: "Sponsor",
+    data () {
+      return {
+        sponsors: []
+      }
+    },
+    methods: {
+      getImageUrl: function (hash) {
+        console.log(hash)
+        return 'https://gateway.ipfs.io/ipfs/' + hash + '/'
+      }
+    },
+    mounted: function () {
+      var self = this
+      let _contract = this.$store.getters.contractInstance()
+      _contract.getPastEvents('beenSponsor', { fromBlock: 0, toBlock: 'latest' }, function(error, events) {
+        events.forEach((element) => {
+          var address=element.returnValues[0]
+          var _sponsor = getSposnsor(address)
+          _sponsor.methods.getSponsor().call().then(function (val) {
+            console.log("Here",val)
+            self.sponsors.push({
+              "name":val[0],
+              "url":val[1],
+              "imageHash":val[2],
+              "duration":val[3],
+            })
+          })
+
+        });
+      })
+    }
+  }
 </script>
 
 <style scoped>
