d5acabe9f08954ea9b2fccda5b5903c587324a3f
Fatih Altuntas <Altuntasfatih42@gmail.com>
2018-07-01 09:03:34 +0300

    Fix sponsor screen

--- dapp/src/components/Sponsor.vue
+++ dapp/src/components/Sponsor.vue
@@ -1,77 +1,13 @@
 <template>
   <div class="container" style="background: sandybrown">
-    </br></br>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="carousel-item active">
-          <div class="row">
-            <div class="col-md-3" v-for="sponsor in sponsors">
-              <div class="thumbnail">
-                <a href="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" class="link">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" alt="Lights" style="width:100%">
-                </a>
-              </div>
-            </div>
-            <div class="col-md-3" v-for="sponsor in sponsors">
-              <div class="thumbnail">
-                <a href="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" class="link">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" alt="Lights" style="width:100%">
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <div class="row">
-            <div class="col-md-3" v-for="sponsor in sponsors">
-              <div class="thumbnail">
-                <a href="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" class="link">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" alt="Lights" style="width:100%">
-                </a>
-              </div>
-            </div>
-            <div class="col-md-3" v-for="sponsor in sponsors">
-              <div class="thumbnail">
-                <a href="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" class="link">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" alt="Lights" style="width:100%">
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <div class="row">
-            <div class="col-md-3" v-for="sponsor in sponsors">
-              <div class="thumbnail">
-                <a href="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" class="link">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" alt="Lights" style="width:100%">
-                </a>
-              </div>
-            </div>
-            <div class="col-md-3" v-for="sponsor in sponsors">
-              <div class="thumbnail">
-                <a href="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" class="link">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Scarlett_Johansson_Césars_2014.jpg" alt="Lights" style="width:100%">
-                </a>
-              </div>
-            </div>
-          </div>
+    <div class="row">
+      <div class="col-md-3" v-for="sponsor in sponsors">
+        <div class="thumbnail">
+          <a :href="sponsor['url']" class="link">
+            <img :src="getImageUrl(sponsor['imageHash'])" alt="Lights" style="width:100%">
+          </a>
         </div>
       </div>
-      </br></br>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
   </div>
 </template>
