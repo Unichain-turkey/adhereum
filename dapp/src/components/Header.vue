6e5df7f7e16ce37a1cbdb831de133a6268464ce6
Fatih Altuntas <Altuntasfatih42@gmail.com>
2018-07-01 17:47:22 +0300

    Slide is added

--- dapp/src/components/Header.vue
+++ dapp/src/components/Header.vue
@@ -16,24 +16,9 @@
       <router-link class="navbar-brand" to="/">
         Adhereum
       </router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="navbar-brand" to="/ipfs">
-            File Upload
+      <router-link class="navbar-brand" to="/admin">
+            Admin
           </router-link>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/">Contact</a>
-          </li>
-        </ul>
-      </div>
     </nav>
   </div>
 </template>
