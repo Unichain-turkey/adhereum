3452f2c011c8034ca0c80acd95157e0e6b18d327
Fatih Altuntas <fatih.altuntas@infonal.com>
2018-11-25 17:48:44 +0300

    Header Update

--- dapp/src/components/Header.vue
+++ dapp/src/components/Header.vue
@@ -7,24 +7,19 @@
     scroll-off-screen
     scroll-target="#scrolling-techniques"
   >
-
     <v-spacer></v-spacer>
 
     <v-toolbar-title class="p-1">
-      <router-link to="demo3">Demo-3</router-link>
+      <router-link class="nav-link white--text" to="demo3">Market Place</router-link>
     </v-toolbar-title>
 
     <v-toolbar-title class="p-1">
-      <router-link to="demo2">Demo-2</router-link>
+      <router-link  class="nav-link white--text" to="demo2">Advertisement</router-link>
     </v-toolbar-title>
 
     <v-toolbar-title class="p-1 pr-2">
-      <router-link to="demo1">Demo-1</router-link>
+      <router-link  class="nav-link white--text" to="demo1">Sponsorship</router-link>
     </v-toolbar-title>
-
-    <v-btn icon>
-      <v-icon>search</v-icon>
-    </v-btn>
 
     <v-btn icon>
       <v-icon>more_vert</v-icon>
@@ -34,12 +29,10 @@
 </template>
 
 <script>
+export default {
+  name: 'Header',
 
-  export default {
-
-    name: 'Header',
-
-  }
+}
 </script>
 
 <style scoped>
