86acfbcbedbd1a191b99d6527b07f5d997f7b9f7
Fatih Altuntas <fatih.altuntas@infonal.com>
2018-12-02 22:23:57 +0300

    Refactor contracts

--- contracts/Sponsor.sol
+++ contracts/Sponsor.sol
@@ -1,59 +1,79 @@
-pragma solidity >=0.4.22 <0.6.0;
+pragma solidity >=0.4.22 < 0.6.0;
 
 import "./Ownable.sol";
 
-contract Sponsor is Ownable{
+contract Sponsor is Ownable {
+
+    enum Type {Gold, Bronze, Silver}
 
     string name;
     string url;
     string imageHash;
+    Type type;
     uint64 startTime;
     uint duration;
     uint deadTime;
 
-    constructor(string _name,string _url,string _imageHash,uint _duration) public
+
+    constructor(string _name, string _url, string _imageHash, uint _type, uint _duration) public
     {
-        name=_name;
-        url=_url;
-        imageHash=_imageHash;
-        startTime=uint64(now);
-        duration=_duration;
-        deadTime=calculateDeadTime(_duration,startTime);
+        name = _name;
+        url = _url;
+        imageHash = _imageHash;
+        startTime = uint64(now);
+        type = Type(_type);
+        duration = _duration;
+        deadTime = calculateDeadTime(_duration, startTime);
 
     }
 
-    function setUrl(string _url)public onlyOwner{
-        url=_url;
+    function setUrl(string _url) public onlyOwner {
+        url = _url;
     }
-    function getUrl()public view returns (string){
+
+    function getUrl() public view returns (string){
         return url;
     }
-    function setHash(string _imageHash)public onlyOwner{
-        imageHash=_imageHash;
+
+    function setHash(string _imageHash) public onlyOwner {
+        imageHash = _imageHash;
     }
-    function getHash()public view returns (string){
+
+    function getHash() public view returns (string){
         return imageHash;
     }
-    function setName(string _name)public onlyOwner{
-        name=_name;
+
+    function setName(string _name) public onlyOwner {
+        name = _name;
     }
-    function getName()public view returns (string){
+
+    function getName() public view returns (string){
         return name;
     }
-    function kill(bool flag) public onlyOwner{
+
+
+    function getType() public view returns (uint){
+        return uint(type);
+    }
+
+    function kill(bool flag) public onlyOwner {
         require(flag);
         selfdestruct(owner);
     }
-    function getSponsor() public view returns(string ,string ,string,uint){
-        return (name, url,imageHash,duration);
+
+    function getSponsor() public view returns (string, string, string,uint, uint){
+        return (name, url, imageHash,uint(type), duration);
     }
-    function getDuration()public view returns (uint){
+
+    function getDuration() public view returns (uint){
         return duration;
     }
-    function getDeadTime()public view returns (uint){
+
+    function getDeadTime() public view returns (uint){
         return deadTime;
     }
-    function calculateDeadTime(uint _duration, uint64 _startTime)public returns (uint)
+
+    function calculateDeadTime(uint _duration, uint64 _startTime) public returns (uint)
     {
         deadTime = _duration + _startTime;
         return deadTime;
