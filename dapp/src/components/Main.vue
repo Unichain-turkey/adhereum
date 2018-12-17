ea29faf594b2d1e91f5421522d60b348fb04338d
Fatih Altuntas <fatih.altuntas@infonal.com>
2018-12-17 20:17:21 +0300

    Main Page is fixed

--- dapp/src/components/Main.vue
+++ dapp/src/components/Main.vue
@@ -1,11 +1,22 @@
 <template>
-    <p>Hii</p>
+  <v-container class="pa-5">
+    <div class="text-xs-center pa-5">
+      <v-btn round color="primary" dark large v-on:click="route('/demo1')">Demo-1</v-btn>
+      <v-btn round color="primary" dark large v-on:click="route('/demo1')">Demo-2</v-btn>
+    </div>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        name: "Main"
+  export default {
+    name: "Main",
+    methods: {
+      route(link) {
+        this.$router.push(link) //named route, see later
+
+      },
     }
+  }
 </script>
 
 <style scoped>
